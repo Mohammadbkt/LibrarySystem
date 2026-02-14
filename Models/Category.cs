ec5dc65ec211ef1c175849e6f10157d9fb655e00
Mohammad Barakat <barakatmohammad455@gmail.com>
2026-02-14 20:47:16 +0300

    adding Category model

---
+++ Models/Category.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace library_system.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string Description { get; set; } = String.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public DateTime? DeletedAt { get; set; }
+
+        // public ICollection<Book> Books { get; set; }
+    }
+}
