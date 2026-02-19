8c13a7d0cadb94b06c5405d0e4cd6e2e36256f32
Mohammad Barakat <barakatmohammad455@gmail.com>
2026-02-19 17:17:26 +0300

    adding User model

---
+++ Models/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace library_system.Models
+{
+    public class User : IdentityUser
+    {
+        string FirstName { get; set; } = String.Empty;
+        string LastName { get; set; } = String.Empty;
+        
+        public bool IsVerified { get; set; }
+        public byte MaxBorrowLimit { get; set; } = 10;
+
+    }
+}
